e0cbc204f250851c96e7ddabbcfb55387bc4c20a
dustinestes <dustin@dustinestes.com>
2026-05-01 10:03:23 -0500

    ch2_l7: complete

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def get_follower_prediction(follower_count, influencer_type, num_months):
-    count = 0
-    match influencer_type:
-        case "fitness":
-            count = follower_count * (4 ** num_months)
-        case "cosmetic":
-            count = follower_count * (3 ** num_months)
-        case _:
-            count = follower_count * (2 ** num_months)
-    
-    return count
+import math
+
+
+def get_influencer_score(num_followers, average_engagement_percentage):
+    return average_engagement_percentage * (math.log(num_followers, 2))
--- main_test.py
+++ main_test.py
@@ -1,42 +1,37 @@
-from main import get_follower_prediction
+from main import *
 
-TestCase = tuple[int, str, int, int]
+run_cases = [(40000, 0.3, 5), (43000, 0.1, 2), (100000, 0.6, 10)]
 
-run_cases: list[TestCase] = [
-    (10, "fitness", 1, 40),
-    (10, "fitness", 2, 160),
-    (12, "cosmetic", 4, 972),
-]
-
-submit_cases: list[TestCase] = run_cases + [
-    (15, "business", 4, 240),
-    (10, "fitness", 5, 10240),
-    (10, "fitness", 6, 40960),
-    (10, "fitness", 7, 163840),
-    (10, "fitness", 8, 655360),
-    (10, "tech", 9, 5120),
+submit_cases = run_cases + [
+    (1, 1, 0),
+    (200, 0.8, 6),
+    (300000, 0.5, 9),
+    (500000, 0.2, 4),
+    (750000, 0.7, 14),
 ]
 
 
-def test(
-    follower_count: int, influencer_type: str, num_months: int, expected: int
-) -> bool:
-    print("---------------------------------")
-    print("Inputs:")
-    print(f" * Follower count: {follower_count}")
-    print(f" * Influencer type: {influencer_type}")
-    print(f" * Number of months: {num_months}")
-    print(f"Expected: {expected}")
-    result = get_follower_prediction(follower_count, influencer_type, num_months)
-    print(f"Actual:   {result}")
-    if result == expected:
-        print("Pass")
-        return True
-    print("Fail")
-    return False
+def test(input1, input2, expected_output):
+    try:
+        print("---------------------------------")
+        print(f"Inputs:")
+        print(f" * num_followers: {input1}")
+        print(f" * average_engagement_percentage: {input2}")
+        result = round(get_influencer_score(input1, input2))
+        print(f"Expected: {expected_output}")
+        print(f"Actual:   {result}")
+        if result == expected_output:
+            print("Pass")
+            return True
+        print("Fail")
+        return False
+    except Exception as e:
+        print("Fail")
+        print(e)
+        return False
 
 
-def main() -> None:
+def main():
     passed = 0
     failed = 0
     skipped = len(submit_cases) - len(test_cases)
@@ -56,7 +51,7 @@ def main() -> None:
         print(f"{passed} passed, {failed} failed")
 
 
-test_cases: list[TestCase] = submit_cases
+test_cases = submit_cases
 if "__RUN__" in globals():
     test_cases = run_cases
 
