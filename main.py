292378e12d6de29cacd43c042eee99f517b7f2ee
dustinestes <dustin@dustinestes.com>
2026-05-06 15:52:54 -0500

    ch5_l04: complete

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-def fib(n):
-    if n <= 1:
-        return n
+def letter_combinations(digits):
+    if not digits:
+        return []
     
-    grandparent = 0
-    parent = 1
-    current = 0
+    result = [""]
 
-    for x in range(n -1):
-        current = parent + grandparent
-        grandparent = parent
-        parent = current
-   
-    return current
+    for digit in digits:
+        if digit not in digit_to_letters:
+            raise ValueError(f"invalid digit: {digit}")
+        
+        new_result = []
+        print(f"letters: {digit} = {digit_to_letters[digit]}")
+        for combo in result:
+            for letter in digit_to_letters[digit]:
+                new_result.append(combo + letter)
+
+        result = new_result
+
+    return result
+
+
+
+
+
+
+
+
+# Don't touch below this line
+
+digit_to_letters = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+}
--- main_test.py
+++ main_test.py
@@ -1,33 +1,62 @@
-from main import *
+from main import letter_combinations
 
-run_cases = [
-    (1, 1),
-    (10, 55),
-    (20, 6765),
+# input; output length; first three output strings; a string that should be present
+TestCase = tuple[str, int, list[str], str]
+
+run_cases: list[TestCase] = [
+    ("", 0, [], ""),
+    ("67", 12, ["mp", "mq", "mr"], "op"),
+    ("43556", 243, ["gdjjm", "gdjjn", "gdjjo"], "hello"),
+    ("2668338", 2187, ["ammtddt", "ammtddu", "ammtddv"], "bootdev"),
 ]
 
-submit_cases = run_cases + [
-    (0, 0),
-    (40, 102334155),
-    (70, 190392490709135),
-    (160, 1226132595394188293000174702095995),
+submit_cases: list[TestCase] = run_cases + [
+    ("420", 0, [], "ValueError"),
+    ("7878326", 3888, ["ptptdam", "ptptdan", "ptptdao"], "rustfan"),
+    ("4568346", 2187, ["gjmtdgm", "gjmtdgn", "gjmtdgo"], "ilovego"),
 ]
 
 
-def test(input1, expected_output):
+def test(
+    digits: str,
+    expected_length: int,
+    expected_initial: list[str],
+    expected_contains: str,
+) -> bool:
     print("---------------------------------")
-    print(f"Input: {input1}")
-    print(f"Expected:  {expected_output}")
-    result = fib(input1)
-    print(f"Actual: {result}")
-    if result == expected_output:
-        print("Pass")
-        return True
+    print(f"Input: '{digits}'")
+    try:
+        result = letter_combinations(digits)
+        print(f"Expected combos: {expected_length}")
+        actual_length = len(result)
+        print(f"Actual combos:   {actual_length}")
+        if expected_length == 0 and actual_length == expected_length:
+            print("Pass")
+            return True
+        print(f"Expected initial combos: {expected_initial}")
+        actual_initial = result[:3]
+        print(f"Actual initial combos:   {actual_initial}")
+        print(f"Expected to contain: '{expected_contains}'")
+        actual_contains = expected_contains in result
+        print(f"Actual contains '{expected_contains}'? {actual_contains}")
+        if (
+            actual_length == expected_length
+            and actual_initial == expected_initial
+            and actual_contains
+        ):
+            print("Pass")
+            return True
+    except ValueError as ve:
+        print(f"Caught ValueError: {ve}")
+        if expected_length == 0 and expected_contains == "ValueError":
+            print("Expected ValueError")
+            print("Pass")
+            return True
     print("Fail")
     return False
 
 
-def main():
+def main() -> None:
     passed = 0
     failed = 0
     skipped = len(submit_cases) - len(test_cases)
@@ -47,7 +76,7 @@ def main():
         print(f"{passed} passed, {failed} failed")
 
 
-test_cases = submit_cases
+test_cases: list[TestCase] = submit_cases
 if "__RUN__" in globals():
     test_cases = run_cases
 
