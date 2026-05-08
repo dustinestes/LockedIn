513dfdb8b47697a80ab5a04a3878e8bd2482d422
dustinestes <dustin@dustinestes.com>
2026-05-08 11:51:25 -0500

    ch9_l06: complete

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from node import Node
 
 
 class LinkedList:
+    def add_to_head(self, node):
+        node.set_next(self.head)
+        self.head = node
+
+    # don't touch below this line
+
     def add_to_tail(self, node):
         if self.head is None:
             self.head = node
             return
-
         last_node = None
         for current_node in self:
             last_node = current_node
-        last_node.next = node
-
-
-    # don't touch below this line
+        last_node.set_next(node)
 
     def __init__(self):
         self.head = None
