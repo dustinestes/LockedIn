5ef1d397a57f9437162115d29524eeada63658de
dustinestes <dustin@dustinestes.com>
2026-05-09 20:38:27 -0500

    ch10_l7: complete

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 class BSTNode:
+    def get_min(self):
+        if self.left is None:
+            return self.val
+        return self.left.get_min()
+
+    def get_max(self):
+        if self.right is None:
+            return self.val
+        return self.right.get_max()
+
+    # don't touch below this line
+
     def __init__(self, val=None):
         self.left = None
         self.right = None
         self.val = val
 
     def insert(self, val):
-        if self.val is None:
+        if not self.val:
             self.val = val
+            return
+
         if self.val == val:
             return
 
-        if val < self.val and self.left is None:
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
             self.left = BSTNode(val)
             return
-        if val < self.val and self.left is not None:
-            self.left.insert(val)
-            return
-        if val > self.val and self.right is None:
-            self.right = BSTNode(val)
-            return
-        if val > self.val and self.right is not None:
+
+        if self.right:
             self.right.insert(val)
             return
+        self.right = BSTNode(val)
